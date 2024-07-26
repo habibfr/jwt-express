e81e94a5f4b605247026356d39441ca0a76548bc
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-07-26 15:22:23 +0700

    clean code

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,16 +1,16 @@
 import jwt from "jsonwebtoken";
-import error_response from "../util/error-response.js";
 
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
   if (token == null) {
-    return res.status(401);
+    return res.sendStatus(401);
   }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403).json(error_response("Token not valid"));
+    if (err) return res.sendStatus(401);
+
     req.email = decoded.email;
     next();
   });
